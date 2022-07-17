8ffb87a65a823411e6fb94ae7b7bfaa89eb7e11e
JH <nojahoon@naver.com>
2022-07-18 01:52:02 +0900

    SQL String,Date Part 추가

---
+++ programmers/sql/String, Date/중성화 여부 파악하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID , NAME,
+CASE
+    WHEN (SEX_UPON_INTAKE like '%Neutered%' or
+           SEX_UPON_INTAKE like '%Spayed%')
+    THEN 'O'
+    ELSE 'X'
+END AS '중성화'
+FROM ANIMAL_INS
