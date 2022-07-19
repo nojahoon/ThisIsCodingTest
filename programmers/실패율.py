c94586a08a371f5b8d7f678c26fe74eaccb9d56b
JH <nojahoon@naver.com>
2022-07-19 18:25:34 +0900

    실패율.py 수정

--- programmers/실패율.py
+++ programmers/실패율.py
@@ -2,16 +2,16 @@ def solution(N, stages):
     dict = {}
     for i in range(1,N+1,1):
         on_stage_not_cleared_count = 0
-        cleared_count = 0
+        on_stage_count = 0
         for each in stages:
             if each == i:
                 on_stage_not_cleared_count+=1
             if each >= i:
-                cleared_count+=1
-        if cleared_count==0:
+                on_stage_count+=1
+        if on_stage_count==0:
             failure = 0
         else:
-            failure = on_stage_not_cleared_count/cleared_count
+            failure = on_stage_not_cleared_count/on_stage_count
         dict[i] = failure
     return sorted(dict,key = lambda x : dict[x] , reverse=True)
 
