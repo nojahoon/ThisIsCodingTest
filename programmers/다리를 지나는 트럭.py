cee20a14ef64191a2250bad0f30b67324b24db3e
nojahoon <nojahoon@naver.com>
2022-09-10 16:33:16 +0900

    다리를 지나는 트럭.py 추가

---
+++ programmers/다리를 지나는 트럭.py
@@ -0,0 +1,16 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = [0 for _ in range(bridge_length)]
+    while bridge:
+        answer += 1
+        bridge.pop(0)
+
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:
+                truck = truck_weights.pop(0)
+                bridge.append(truck)
+            else:
+                bridge.append(0)
+    return answer
+
+
