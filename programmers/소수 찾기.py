3e48402fde289d919d9a7da1d52bce39ad80306b
nojahoon <nojahoon@naver.com>
2022-09-20 23:27:22 +0900

    소수 찾기.py 풀이수정

--- programmers/소수 찾기.py
+++ programmers/소수 찾기.py
@@ -1,16 +1,21 @@
-import math
-
+from itertools import permutations
 
 def solution(n):
-    answer = 0
-    for i in range(2, n + 1, 1):
-        IS_PRIME = True
-        root_number = int(math.sqrt(i)) + 1
-        for j in range(2, root_number, 1):
-            if i % j == 0:
-                IS_PRIME = False
+    answer=0
+    num = [i for i in n]
+    per = []
+    for i in range(1,len(num)+1):
+        per+=list(permutations(num,i))
+    num = [int(("").join(i)) for i in per]
+    num = list(set(num)) #중복요소 제거
+    for check_num in num:
+        if check_num<2:
+            continue
+        for i in range(2,int(check_num**0.5)+1,1):
+            if check_num%i==0:
                 break
-        if IS_PRIME:
-            answer += 1
-
+        else: #소수
+            answer+=1
     return answer
+
+print(solution("011"))
